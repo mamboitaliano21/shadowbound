7f328d3a769c3c2effeef2387bc81f40462790b3
zelfrontial <denisthamrin@gmail.com>
2014-10-08 13:29:05 -0700

    commit texturegameobject

---
+++ TextureGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpDX;
+using SharpDX.Toolkit;
+
+namespace Lab
+{
+    using SharpDX.Toolkit.Graphics;
+    abstract public class TextureGameObject : GameObject
+    {
+        public Buffer<VertexPositionNormalTexture> vertices;
+    }
+}
