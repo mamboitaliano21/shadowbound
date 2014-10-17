86a9f8fef221324de1052c38ef750fc7c7cea7a9
erlanggagama <erlangga.gama@gmail.com>
2014-10-17 15:26:17 +1100

    get width and height landscape methods added

--- Landscape.cs
+++ Landscape.cs
@@ -13,40 +13,35 @@ namespace Lab
         private Matrix World;
         private Matrix WorldInverseTranspose;
 
-        private int mapWidth = 513;
-        private int mapHeight = 513;
-        private int mountainHeight = 200;
+        private static float LANDSCAPEHEIGHT = 0.0f;
+        private static float LANDSCAPEWIDTH = 200.0f;
 
-        private Vector3 initialPos = new Vector3(0, 100, -20);
-        private Vector3 initialCamera = new Vector3(0, 100, 0);
+        //private Vector3 initialPos = new Vector3(0, 100, -20);
+        //private Vector3 initialCamera = new Vector3(0, 100, 0);
+
+        public float getHeight()
+        {
+            return LANDSCAPEHEIGHT;
+        }
+
+        public float getWidth()
+        {
+            return LANDSCAPEWIDTH;
+        }
 
         public Landscape(LabGame game)
         {
             this.game = game;
             type = GameObjectType.Landscape;
-            //H-value,mapwidth,mapheight,iteration,maximum depth,maximum height
-            //TerrainGenerator tg = new TerrainGenerator(200);
-            //float[,] arr = tg.generateTerrain();
-            //VertexPositionNormalColor[] landscapeVertices = tg.generateVertex(arr);
-
-
-            /*List<VertexPositionNormalColor> landscapeVertices = new List<VertexPositionNormalColor>();
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(0.0f, 10.0f,0.0f), new Vector3(0.0f, 1.0f,0.0f), Color.Green));
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(0.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), Color.Green));
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(200.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), Color.Green));
-            
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(0.0f, 10.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), Color.Green));
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(200.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), Color.Green));
-            landscapeVertices.Add(new VertexPositionNormalColor(new Vector3(200.0f, 10.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), Color.Green));
-            */
+   
             List<VertexPositionNormalTexture> landscapeVertices = new List<VertexPositionNormalTexture>();
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, 10.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f,0.0f)));
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(200.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, LANDSCAPEHEIGHT, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f,0.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, LANDSCAPEHEIGHT, LANDSCAPEWIDTH), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 0.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(LANDSCAPEWIDTH, LANDSCAPEHEIGHT, LANDSCAPEWIDTH), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
 
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, 10.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 0.0f)));
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(200.0f, 10.0f, 200.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
-            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(200.0f, 10.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(0.0f, LANDSCAPEHEIGHT, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 0.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(LANDSCAPEWIDTH, LANDSCAPEHEIGHT, LANDSCAPEWIDTH), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(1.0f, 1.0f)));
+            landscapeVertices.Add(new VertexPositionNormalTexture(new Vector3(LANDSCAPEWIDTH, LANDSCAPEHEIGHT, 0.0f), new Vector3(0.0f, 1.0f, 0.0f), new Vector2(0.0f, 1.0f)));
            
             vertices = Buffer.Vertex.New(
                 game.GraphicsDevice,
@@ -67,6 +62,7 @@ namespace Lab
         public override void Draw(GameTime gameTime, Effect effect)
         {
             this.effect = effect;
+
             // Setup the effect parameters
             this.effect.Parameters["World"].SetValue(World);
             this.effect.Parameters["worldInvTrp"].SetValue(WorldInverseTranspose);
@@ -74,44 +70,12 @@ namespace Lab
             // Setup the vertices
             this.game.GraphicsDevice.SetVertexBuffer(vertices);
             this.game.GraphicsDevice.SetVertexInputLayout(inputLayout);
-
-            // Apply the basic effect technique and draw the rotating cube
-            //effect.CurrentTechnique.Passes[0].Apply();
-            
-            
+    
             foreach(EffectPass pass in this.effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
                 this.game.GraphicsDevice.Draw(PrimitiveType.TriangleList, vertices.ElementCount);
-                /*this.game.GraphicsDevice.Draw(PrimitiveType.TriangleList, vertices, 0, 1, 
-                    VertexPositionNormalTexture.VertexDeclaration);*/
             }
-            
-            //this.game.GraphicsDevice.Draw(PrimitiveType.TriangleList, vertices.ElementCount);
-        }
-
-        private Color decideColor(double height)
-        {
-            Color textureColor = Color.Green;
-            if (height < mountainHeight / 4)
-            {
-                textureColor = Color.Green;
-            }
-            else if (height < mountainHeight / 2)
-            {
-                textureColor = Color.Yellow; 
-            }
-            else if (height < mountainHeight / 1.5)
-            {
-                
-                textureColor = Color.Gray;
-            }
-            else 
-            {
-                textureColor = Color.Snow;
-            }
-
-            return textureColor;
         }
     }
 }
